88b70459b6b9a1a0ce02db466de2a54668dab5e6
Santi <santinarta101@gmail.com>
2023-05-30 20:22:39 -0300

    Se definen estructuras del pong

--- Userland/pong.c
+++ Userland/pong.c
@@ -1,3 +1,33 @@
-//
-// Created by Santi on 5/30/2023.
-//
+// tamaño de pantalla
+// 1024 x 768
+
+typedef struct
+{
+    int x;
+    int y;
+    int posx;
+    int posy;
+
+} Ball;
+
+typedef struct
+{
+    int y;
+    int length;
+} Bars;
+
+typedef struct
+{
+    Bars bar;
+    int score;
+} Player;
+
+typedef struct{
+    Player player1;
+    Player player2;
+    Ball ball;
+} Game;
+
+/* Para la pelota hay que agregar un draw, updatePosition y detector de tocar la barra
+ *  Mismo para las barras
+ */
