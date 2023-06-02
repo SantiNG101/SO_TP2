db4c4f1584d374fb72b8519cc0ac54d32761d153
Tobias Juhasz <108370047+juhasztobias@users.noreply.github.com>
2023-06-02 00:26:12 -0300

    Se agrega funcionalidad de CapLock

--- Kernel/include/keyboard.h
+++ Kernel/include/keyboard.h
@@ -5,6 +5,10 @@
 #define KERNEL_KEYBOARD_HANDLER_H
 #include <naiveConsole.h>
 
+#define toMinus(c) (c | 0x20)
+#define isalpha(c) (toMinus(c) >= 'a' && toMinus(c) <= 'z')
+#define toMayus(c) (isalpha(c) ? c & 0x5F : c)
+
 /*
  * TODO: Documentar correctamente.
  */
