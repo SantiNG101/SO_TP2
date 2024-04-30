5f200103f7f4f10654f2f238680a32b1e96ed4fa
Sergio-Smirnoff <sergi.smirnoff@gmail.com>
2024-04-30 17:54:54 -0300

    Adding simple memory manager without freeing

---
+++ Kernel/include/mm.h
@@ -0,0 +1,9 @@
+#ifndef _DEFS_H_
+#define _DEFS_H_
+#include <stdint.h>
+#include <stdarg.h>
+
+#define MEM_START 0xA00000;
+#define MEM_STOP 0x1F600000;
+
+#endif
---
+++ Kernel/mm.c
@@ -0,0 +1,37 @@
+#include <mm.h>
+
+/*
+typedef struct mem_block{
+    char* start;
+    char* end;
+    char used;
+}block;
+
+void* memalloc(uint64_t size);
+void free(void* mem);
+
+char memarray[521142272] = {0};
+*/
+
+char* start_mem = MEM_START;
+char* current_mem;
+char* stop_mem = MEM_STOP;
+
+void mem_initialize(){
+    current_mem = start_mem;
+}
+
+
+void* memalloc(uint64_t size){
+    if ( current_mem == stop_mem || current_mem + size > stop_mem )
+        return -1; // return cant assign
+    char* aux = current_mem;
+    current_mem +=size;
+    return aux;
+
+}
+
+void free(void* mem){
+    //not implemented yet
+    return;
+}
