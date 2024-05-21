5699c4f41c131457cb6170125582b59f6de0de5e
Mica Perillo <micaelaperillo26@gmail.com>
2024-05-21 09:57:07 -0300

    first version of memory manager

--- Kernel/include/mm.h
+++ Kernel/include/mm.h
@@ -5,11 +5,16 @@
 #include <stdarg.h>
 
 #define MEM_START 0x50000;
-#define MEM_STOP 0x9FFFF;
+#define MEM_END 0x9FFFF;
+#define ERROR -1
+#define CHUNK_SIZE 4096 // tbd
+#define MAX_CHUNKS (MEM_END - MEM_START) / CHUNK_SIZE
 
+#define TRUE 1
+#define FALSE 0
 
-void mem_initialize();
-void* memalloc(int size);
-void free(void* mem);
+memManamemManagerADT mem_initialize(int chunk_size);
+void * mem_alloc(memManagerADT mem_manager, int size);
+void * free(memManagerADT mem_manager, void * allocated_memory);
 
 #endif
--- Kernel/mm.c
+++ Kernel/mm.c
@@ -1,4 +1,5 @@
 #include <mm.h>
+#include <stddef.h>
 
 /*
 typedef struct mem_block{
@@ -13,25 +14,113 @@ void free(void* mem);
 char memarray[521142272] = {0};
 */
 
-char* start_mem = MEM_START;
-char* current_mem;
-char* stop_mem = MEM_STOP;
+typedef struct memManagerCDT {
+    char * start;
+    char * end;
+    char * current_address;
+    int chunk_size;
+    memChunkADT chunks[MAX_CHUNKS]; // NOT a freelist, it's a list of all possible chunks
+    int current_chunk;
+} memManagerCDT;
 
-void mem_initialize(){
-    current_mem = start_mem;
+typedef struct memManagerCDT * memManagerADT;
+
+typedef struct memChunkCDT {
+    int used;
+    char * starts_at;
+    int length;
+} memChunkCDT;
+
+typedef struct memChunkADT * memChunkADT;
+
+memManagerADT mem_initialize(int chunk_size) {
+    if (chunk_size < 1 || (MEM_END - MEM_START) < chunk_size)
+        return ERROR;
+
+    memManagerADT mem_manager = (memManagerADT) MEM_START;
+
+    mem_manager->start = MEM_START;
+    mem_manager->end = MEM_END;
+    mem_manager->chunk_size = chunk_size;
+
+    // initialize all posible chunks
+    for (int i = 0; i < MAX_CHUNKS; i++) {
+        memChunkADT new_chunk = (memChunkADT) (MEM_START + sizeof(memManager) + (i * chunk_size));
+        new_chunk->used = FALSE;
+        new_chunk->starts_at = MEM_START + sizeof(memManager) + (i * chunk_size);
+        new_chunk->length = chunk_size;
+        
+        mem_manager->chunks[i] = new_chunk;
+    }
+
+    mem_manager->current_address = MEM_START + sizeof(memManager) + sizeof(memChunk) * MAX_CHUNKS;
+
+    // first chunk -> memory manager with the list of chunks
+    mem_manager->chunks[0]->used = TRUE;
+    mem_manager->chunks[0]->starts_at = MEM_START;
+    mem_manager->chunks[0]->length = sizeof(memManager) + sizeof(memChunk) * MAX_CHUNKS;
+
+    mem_manager->current_chunk = 1;
+
+    return mem_manager;    
 }
 
+void * mem_alloc(memManagerADT mem_manager, int size) {
+    if (mem_manager->current == MEM_END || (mem_manager->current + size) > MEM_END)
+        return ERROR;
 
-void* memalloc(int size){
-    if ( current_mem == stop_mem || current_mem + size > stop_mem )
-        return -1; // return cant assign
-    char* aux = current_mem;
-    current_mem +=size;
-    return aux;
+    char * direction = mem_manager->current_address;
 
+    mem_manager->current += size; // TODO: calculate log size in buddy here
+
+    int chunks_needed = size / mem_manager->chunk_size;
+    if (size % mem_manager->chunk_size != 0)
+        chunks_needed++;
+
+    // TODO: search for the first chunk that fits
+    while (int i = 0; i < MAX_CHUNKS; i++) {
+
+        if (mem_manager->chunks[i]->used == TRUE)
+            continue;
+
+        int j = 0;
+        while (j < chunks_needed) {
+            if (mem_manager->chunks[i+j]->used == TRUE)
+                break;
+            j++;
+        }
+
+        if (j == chunks_needed)
+            // found a chunk that fits from [i, i+j]
+
+            direction = mem_manager->chunks[i]->starts_at;
+            
+            for (int k = i; k < chunks_needed; k++) {
+                mem_manager->chunks[i+k]->used = TRUE;
+                mem_manager->chunks[i+k]->length = size;
+            }
+    }
+
+    mem_manager->current_chunk++; // not used
+    mem_manager->current_address += size;
+
+    return direction;
 }
 
-void free(void* mem){
-    //not implemented yet
-    return;
+void * free(memManagerADT mem_manager, void * allocated_memory) {
+
+    if (allocated_memory == NULL || allocated_memory < MEM_START || allocated_memory > MEM_END)
+        return ERROR;
+
+    for (int i = 0; i < MAX_CHUNKS: i++) {
+        if (mem_manager->chunks[i]->starts_at == allocated_memory) {
+            int chunk_length = mem_manager->chunks[i]->length;
+            for (int j = 0; j < chunk_length; j++) {
+                mem_manager->chunks[i+j]->used = FALSE;
+                mem_manager->chunks[i+j]->length = 0;
+            }
+            return;
+        }
+    }
+    return ERROR;
 }
